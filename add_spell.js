5bd100a29d014dd90ac22cccfb1540056ccf43b1
Millo <23leeuw@gmail.com>
2024-10-18 22:19:19 +0200

    2

--- add_spell.js
+++ add_spell.js
@@ -1,12 +1,48 @@
 import { CATEGORIES, RUNE_COMPONENTS, RUNE_ARCHITECTURES } from "./static.js";
 
 
+var selectedComponents = [];
+var selectedCategories = [];
+var selectedArchitecture = "";
+
 const main = () => {
     console.log(RUNE_COMPONENTS)
     console.log(CATEGORIES)
     console.log(RUNE_ARCHITECTURES)
 
-    
+    const componentsContainer = document.getElementById("components");
+    const categoriesContainer = document.getElementById("categories");
+    const architecturesContainer = document.getElementById("architecture");
+
+    // Components
+    RUNE_COMPONENTS.forEach((component) => {
+        const componentDiv = document.createElement("div");
+        componentDiv.innerHTML = component;
+        componentDiv.classList.add("selectable");
+        componentDiv.addEventListener("click", () => {
+            if (selectedComponents.includes(component)) {
+                selectedComponents = selectedComponents.filter((c) => c !== component);
+                componentDiv.classList.remove("selected");
+            } else {
+                selectedComponents.push(component);
+                componentDiv.classList.add("selected");
+            }
+        });
+        componentsContainer.appendChild(componentDiv);
+    });
+
+    // Architecture
+    RUNE_ARCHITECTURES.forEach((architecture) => {
+        const architectureDiv = document.createElement("div");
+        architectureDiv.innerHTML = architecture;
+        architectureDiv.classList.add("selectable");
+        architectureDiv.addEventListener("click", () => {
+            selectedArchitecture = architecture;
+            architecturesContainer.querySelectorAll(".selectable").forEach((a) => a.classList.remove("selected"));
+            architectureDiv.classList.add("selected");
+        });
+        architecturesContainer.appendChild(architectureDiv);
+    });
 }
 
 
--- static.js
+++ static.js
@@ -1,4 +1,14 @@
-export const RUNE_COMPONENTS = ["Fire", "Water", "Earth", "Air", "Life", "Death", "Order", "Chaos", "Illusion", "Necromancy"]; // more to be added
 export const RUNE_ARCHITECTURES = ["Enhancing", "Transmutation", "Emission", "Manipulation", "Conjuration", "Specialization"]; // confined to only these
-export const CATEGORIES = ["Combat", "Utility", "Healing", "Buff", "Debuff", "Control"];
+export const RUNE_COMPONENTS = [
+    "Lightning", "Fire", "Air", "Water",
+    "Advanced Locking", "Necromancy", "Earth",
+    "Illusionary", "Space", "Nature", "Monde",
+    "Instantiate", "Sealing", "Psychic", "Protecting",
+    "Harming", "Shadow", "Holy", "Unholy", "Explosion",
+    "Metal", "Cold", "Oil", "Literature", "Locking", 
+    "Light", "Dark", "Sound", "Time", "Force", "Life"
+]; // more to be added
+export const CATEGORIES = [
+    "Combat", "Utility", "Healing", "Buff", "Debuff", "Control"
+]; // more to be added
 
